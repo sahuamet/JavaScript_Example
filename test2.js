495bc3ca5786b9eb8e4dfa4191aa584c1c4a072a
sahuamitp <sahuamitp@gmail.com>
2022-04-10 20:55:14 +0530

    JS Example

---
+++ test2.js
@@ -0,0 +1,27 @@
+const num = 123;
+var str="";
+var cnt=0;
+
+const placeValue = (num, res = [], factor = 1) => {
+   if(num){
+      const val = (num % 10) * factor;
+      res.unshift(val);
+      return placeValue(Math.floor(num / 10), res, factor * 10);
+   };
+   return res;
+};
+
+var temp=placeValue(num);
+temp.forEach(element => {
+    str=str+element;
+});
+
+for(let i=0;i<str.length;i++)
+{
+    if(str[i]=='0')
+    {
+        cnt++;
+    }
+}
+console.log(str);
+console.log(cnt);
